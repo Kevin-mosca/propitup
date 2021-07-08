d3d8e61e7b3f2ea9a3f68f60881ecc3ca128909d
Kevin-mosca <Kevinmoscaa@gmail.com>
2021-07-07 17:17:08 -0700

    comit

--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 class MyNewComponent extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    haveBirthday = () => {
+        this.setState({ age: this.state.age+1})
+    }
     render(){
         const {firstName, lastName, age, color} = this.props;
         return(
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {color}</p>
+                <button onClick={ this.haveBirthday } >Birthday Button for {firstName} {lastName}</button>
             </div>
         );
     }
